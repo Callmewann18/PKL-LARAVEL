4b8b8bf3685c433ff4f948a16d7732108d82c04b
Callmewann18 <140819101+Callmewann18@users.noreply.github.com>
2024-11-22 10:21:12 +0700

    Update create.blade.php

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -15,35 +15,43 @@
             @endif
             <form action="/posts/store" method="post" enctype="multipart/form-data">
                 @csrf
-                <div class="mb-3">
-                    <label for="title" class="form-label">Title</label>
-                    <input class="form-control" type="text" name="title" id="title" value="{{old('title')}}">
-                    @if ($errors->has('title'))
-                    <p class="text-danger">{{$errors->first('title')}}</p>
-                        
-                    @endif
-                </div>
-                <div class="mb-3">
-                    <label for="description" class="form-label">Description</label>
-                    <textarea name="description" id="description" cols="5" rows="5" class="form-control">{{old('description')}}</textarea>
-                    @if ($errors->has('description'))
-                    <p class="text-danger">{{$errors->first('description')}}</p>
-                        
-                    @endif
-                </div>
-                <div class="mb-3">
-                    <label for="image" class="form-label">Image</label>
-                    <input class="form-control" type="file" name="image" id="image">
-                    @if ($errors->has('image'))
-                    <p class="text-danger">{{$errors->first('image')}}</p>
-                        
-                    @endif
-                </div>
-                <div class="mb-3">
-                    <button class="btn btn-success">Create Posts</button>
-                </div>
+                <table class="table table-borderless">
+                    <tbody>
+                        <tr>
+                            <td><label for="title" class="form-label">Title</label></td>
+                            <td>
+                                <input class="form-control" type="text" name="title" id="title" value="{{ old('title') }}">
+                                @if ($errors->has('title'))
+                                    <p class="text-danger">{{ $errors->first('title') }}</p>
+                                @endif
+                            </td>
+                        </tr>
+                        <tr>
+                            <td><label for="description" class="form-label">Description</label></td>
+                            <td>
+                                <textarea name="description" id="description" cols="5" rows="5" class="form-control">{{ old('description') }}</textarea>
+                                @if ($errors->has('description'))
+                                    <p class="text-danger">{{ $errors->first('description') }}</p>
+                                @endif
+                            </td>
+                        </tr>
+                        <tr>
+                            <td><label for="image" class="form-label">Image</label></td>
+                            <td>
+                                <input class="form-control" type="file" name="image" id="image">
+                                @if ($errors->has('image'))
+                                    <p class="text-danger">{{ $errors->first('image') }}</p>
+                                @endif
+                            </td>
+                        </tr>
+                        <tr>
+                            <td colspan="2">
+                                <button class="btn btn-success">Create Posts</button>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
             </form>
         </div>
     </div>
-    
 @endsection
