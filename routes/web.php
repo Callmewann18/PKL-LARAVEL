be8b3cbfbd6494ef4d3028b5898144defac9b46d
Callmewann18 <140819101+Callmewann18@users.noreply.github.com>
2024-11-22 10:13:54 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\GalleryController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ContactController;
+use App\Models\Post;
+use App\Models\Gallery;
+use App\Mail\HelloMail;
+use Illuminate\Support\Facades\Mail;
 
 Route::get('/', function () {
     return view('welcome');
@@ -18,11 +24,19 @@ Route::get('/tentang', function () {
 });
 
 Route::get('/berita', function () {
-    return view('berita');
+    $posts = Post::all();
+    $posts_count = $posts->count();
+    return view('berita', [
+        'posts' => $posts->random($posts_count > 5 ? 5 : $posts_count)->collect(),
+    ]);
 });
 
-Route::get('/galery', function (){
-    return view('galery');
+Route::get('/galery', function () {
+    $gallery = Gallery::all();
+    $gallery_count = $gallery->count();
+    return view('galery', [
+        'gallery' => $gallery->random($gallery_count > 5 ? 5 : $gallery_count)->collect(),
+    ]);
 });
 
 Route::get('/kontak', function (){
@@ -49,9 +63,20 @@ Route::get('posts/{post}/edit',[PostController::class,'edit']);
 Route::put('posts/{post}/update',[PostController::class,'update']);
 Route::delete('posts/{post}/delete',[PostController::class,'destroy']);
 
+Route::get('gallery',[GalleryController::class,'index']);
+Route::get('gallery/create',[GalleryController::class,'create']);
+Route::post('gallery/store',[GalleryController::class,'store']);
+Route::get('gallery/{post}/show',[GalleryController::class,'show']);
+Route::get('gallery/{post}/edit',[GalleryController::class,'edit']);
+Route::put('gallery/{post}/update',[GalleryController::class,'update']);
+Route::delete('gallery/{post}/delete',[GalleryController::class,'destroy']);
+
 Route::group(['middleware' => 'auth'], function () {
 
     Route::get('/dashboard', [HomeController::class, 'index']); 
     Route::delete('/logout', [AuthController::class, 'logout'])->name('logout');
 });
+
+Route::post('/contact', [ContactController::class, 'sendContactForm'])->name('contact.send');
+
 
